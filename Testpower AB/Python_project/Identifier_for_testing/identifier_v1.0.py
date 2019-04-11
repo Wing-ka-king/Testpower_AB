1ee8635387fafac967030bd4cfcbf437660d72ea
Wing-ka-king <singhakash1298@gmail.com>
2019-04-12 01:49:59 +0200

    Testing version ready. Would be itiratively improved

--- Testpower AB/Python_project/Identifier_for_testing/identifier_v1.0.py
+++ Testpower AB/Python_project/Identifier_for_testing/identifier_v1.0.py
@@ -13,8 +13,7 @@ def message_array(hex_data, start_bit=2):
 
 def signedhex(val, nbits):
     val = hex((val + (1 << nbits)) % (1 << nbits))
-    return '0x' + val[2:].zfill(int(nbits/4))
-
+    return '0x' + val[2:].zfill(int(nbits / 4))
 
 
 class Parser(object):
@@ -70,7 +69,7 @@ class Parser(object):
                 byte_size = 3
                 self.row_data[i] = int(float(self.row_data[i]) * 1000)
                 # hex_data1 = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
-                hex_data = signedhex(self.row_data[i], byte_size*8)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
                 to_can_msg = message_array(hex_data)
                 print(Message(arbitration_id=104, data=to_can_msg))
 
@@ -162,7 +161,7 @@ class Parser(object):
                 byte_size = 3
                 self.row_data[i] = int(float(self.row_data[i]) * 1000)
                 # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
-                hex_data = signedhex(self.row_data[i], byte_size*8)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
                 to_can_msg = message_array(hex_data)
                 print(Message(arbitration_id=114, data=to_can_msg))
 
@@ -276,7 +275,7 @@ class Parser(object):
                     hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
                     to_can_msg = message_array(hex_data)
                     print(Message(arbitration_id=124, data=to_can_msg))
-                    pass    # check the use of pass
+                    pass  # check the use of pass
 
             elif self.headers[i] == 'Uppeak-1':  # ID 125 byte size = 3
                 to_can_msg = []
@@ -292,7 +291,7 @@ class Parser(object):
                 byte_size = 3
                 self.row_data[i] = int(float(self.row_data[i]) * 1000)
                 # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
-                hex_data = signedhex(self.row_data[i], byte_size*8)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
                 to_can_msg = message_array(hex_data)
                 print(Message(arbitration_id=126, data=to_can_msg))
 
@@ -314,15 +313,233 @@ class Parser(object):
                 to_can_msg = message_array(hex_data)
                 print(Message(arbitration_id=128, data=to_can_msg))
 
-        return to_can_msg
+            elif self.headers[i] == 'Pppeak-1':  # ID 129 byte size = 4
+                to_can_msg = []
+                byte_size = 4
+                self.row_data[i] = int(float(self.row_data[i]) * 1000)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=129, data=to_can_msg))
 
-    # def tally(self):
-    #     self.listed_param = []
-    #     for i in self.headers:
-    #         for j in self.all_headers:
-    #             if i == j:
-    #                 self.listed_param.append(i)
-    #     return self.listed_param
+            elif self.headers[i] == 'Pmpeak-1':  # ID 130 byte size = 4
+                to_can_msg = []
+                byte_size = 4
+                self.row_data[i] = int(float(self.row_data[i]) * 1000)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=130, data=to_can_msg))
+
+            elif self.headers[i] == 'MATH':  # ID 131 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10000)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=131, data=to_can_msg))
+
+            elif self.headers[i] == 'TIME':  # ID 132 byte size = 4
+                to_can_msg = []
+                byte_size = 4
+                if self.row_data[i] == 'NAN':
+                    print(Message(arbitration_id=132,data=[0xff, 0xff, 0xff, 0xfe]))
+                    pass
+                else:
+                    time_play = int(float(self.row_data[i].replace(":", "")) * 1000)
+                    hex_data = '0x{0:0{1}X}'.format(time_play, byte_size * 2)
+                    # hex_data = signedhex(self.row_data[i], byte_size * 8)
+                    to_can_msg = message_array(hex_data)
+                    print(Message(arbitration_id=132, data=to_can_msg))
+                    pass
+
+            elif self.headers[i] == 'WP-1':  # ID 133 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E8)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=133, data=to_can_msg))
+
+            elif self.headers[i] == 'WPp-1':  # ID 134 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E8)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=134, data=to_can_msg))
+
+            elif self.headers[i] == 'WPm-1':  # ID 135 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E8)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=135, data=to_can_msg))
+
+            elif self.headers[i] == 'q-1':  # ID 136 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E6)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=136, data=to_can_msg))
+
+            elif self.headers[i] == 'qp-1':  # ID 135 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E6)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=135, data=to_can_msg))
+
+            elif self.headers[i] == 'qm-1':  # ID 136 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E6)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=136, data=to_can_msg))
+
+            elif self.headers[i] == 'U(k)-1-Total':  # ID 139 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E3)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=139, data=to_can_msg))
+
+            elif self.headers[i] == 'I(k)-1-Total':  # ID 140 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E3)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=140, data=to_can_msg))
+
+            elif self.headers[i] == 'P(k)-1-Total':  # ID 141 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E3)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=141, data=to_can_msg))
+
+            elif self.headers[i] == 'Uhdf(k)-1-Total':  # ID 142, byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                if self.row_data[i] == 'NAN':
+                    print(Message(arbitration_id=142, data=b'NAN'))
+                    pass
+                else:
+                    self.row_data[i] = int(float(self.row_data[i]) * 1000)
+                    # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                    hex_data = signedhex(self.row_data[i], byte_size * 8)
+                    to_can_msg = message_array(hex_data)
+                    print(Message(arbitration_id=142, data=to_can_msg))
+                    pass
+
+            elif self.headers[i] == 'Ihdf(k)-1-Total':  # ID 143, byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                if self.row_data[i] == 'NAN':
+                    print(Message(arbitration_id=143, data=b'NAN'))
+                    pass
+                else:
+                    self.row_data[i] = int(float(self.row_data[i]) * 1000)
+                    # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                    hex_data = signedhex(self.row_data[i], byte_size * 8)
+                    to_can_msg = message_array(hex_data)
+                    print(Message(arbitration_id=143, data=to_can_msg))
+                    pass
+
+            elif self.headers[i] == 'Phdf(k)-1-Total':  # ID 144, byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                if self.row_data[i] == 'NAN':
+                    print(Message(arbitration_id=144, data=b'NAN'))
+                    pass
+                else:
+                    self.row_data[i] = int(float(self.row_data[i]) * 1000)
+                    # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                    hex_data = signedhex(self.row_data[i], byte_size * 8)
+                    to_can_msg = message_array(hex_data)
+                    print(Message(arbitration_id=144, data=to_can_msg))
+                    pass
+
+            elif self.headers[i] == 'Uthd-1':  # ID 145 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                self.row_data[i] = int(float(self.row_data[i]) * 10E4)
+                # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=145, data=to_can_msg))
+
+            elif self.headers[i] == 'Ithd-1':  # ID 146 byte size = 3
+                to_can_msg = []
+                byte_size = 3
+                if self.row_data[i]=='Error    ':
+                    print(Message(arbitration_id=146, data = [0xff,0xff,0xff]))
+                    pass
+                else:
+                    self.row_data[i] = int(float(self.row_data[i]) * 10E4)
+                    # hex_data = '0x{0:0{1}X}'.format(self.row_data[i], byte_size * 2)
+                    hex_data = signedhex(self.row_data[i], byte_size * 8)
+                    to_can_msg = message_array(hex_data)
+                    print(Message(arbitration_id=146, data=to_can_msg))
+                    pass
+
+            elif self.headers[i] == 'MeasureMode':  # ID 147 byte size = 1
+                to_can_msg = []
+                byte_size = 1
+                hex_data = '0x{0:0{1}X}'.format(int(self.row_data[i]), byte_size * 2)
+                # hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=147, data=to_can_msg))
+
+            elif self.headers[i] == 'DisplayMode':  # ID 148 byte size = 1
+                to_can_msg = []
+                byte_size = 1
+                hex_data = '0x{0:0{1}X}'.format(int(self.row_data[i]), byte_size * 2)
+                # hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=148, data=to_can_msg))
+
+            elif self.headers[i] == 'IntegrateMode':  # ID 149 byte size = 1
+                to_can_msg = []
+                byte_size = 1
+                hex_data = '0x{0:0{1}X}'.format(int(self.row_data[i]), byte_size * 2)
+                # hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=149, data=to_can_msg))
+
+            elif self.headers[i] == 'UpdateRate':  # ID 150 byte size = 1
+                to_can_msg = []
+                byte_size = 1
+                hex_data = '0x{0:0{1}X}'.format(int(self.row_data[i]), byte_size * 2)
+                # hex_data = signedhex(self.row_data[i], byte_size * 8)
+                to_can_msg = message_array(hex_data)
+                print(Message(arbitration_id=150, data=to_can_msg))
+
+            elif self.headers[i] == '---------':
+                pass
+
+            else:
+                print(self.headers[i], self.row_data[i], "New condition encountered")
+                input()
+
+        return to_can_msg
 
 
 class FileTailer(object):
@@ -353,4 +570,4 @@ for row in csv_reader:
     print("Read row: %r" % (row,))
     csv_data = Parser(row, colNames)
     csv_data.data_assign()
-    break
+    # break
